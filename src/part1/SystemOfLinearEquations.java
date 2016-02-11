258d67127b2a1068d5636262e91bf93b9c5dafc8
melika barzegaran hosseini <melika.barzegaran.hosseini@gmail.com>
2016-02-12 00:38:35 +0330

    Add calculating determinant to the system of linear equations
    the method gets a square matrix and computes the determinant of it
    recursively; then returns it. some polishing were added as well.

--- src/part1/SystemOfLinearEquations.java
+++ src/part1/SystemOfLinearEquations.java
@@ -23,12 +23,13 @@ public class SystemOfLinearEquations
         }
 
         BufferedReader reader = null;
+        Input input = new Input();
         try
         {
             reader = new BufferedReader(new FileReader(path));
 
             String line;
-            Integer rank = null;
+            Integer rank;
             if((line = reader.readLine()) != null && !line.isEmpty())
             {
                 try
@@ -44,25 +45,24 @@ public class SystemOfLinearEquations
                     System.err.println("error: the first line of the file '" + path + "' is not structured properly" +
                             ".\n the first line should represent the rank of the matrix coefficient.\n the rank " +
                             "number must be a positive number with the max value of '"+ Integer.MAX_VALUE + "'.");
-                    System.exit(1);
+                    throw new Exception();
                 }
             }
             else
             {
                 System.err.println("error: the first line of the file '" + path + "' is null or empty.");
-                System.exit(1);
+                throw new Exception();
             }
 
             System.out.println("the rank of the matrix coefficient is '" + rank + "'.");
 
-            Input input = new Input();
             input.coefficients = new Integer[rank][rank];
 
             for(int i = 0; i < rank; i++)
             {
                 if((line = reader.readLine()) != null && !line.isEmpty())
                 {
-                    String[] tokens = line.split("\\s+");
+                    String[] tokens = line.trim().split("\\s+");
                     if(tokens.length == rank)
                     {
                         for(int j = 0; j < rank; j++)
@@ -77,7 +77,7 @@ public class SystemOfLinearEquations
                                         " not structured properly.\n lines should represent the values of the matrix " +
                                         "coefficient.\n the values must be integer numbers with the min value of '" +
                                         Integer.MIN_VALUE + "' and the max value of '" + Integer.MAX_VALUE + "'.");
-                                System.exit(1);
+                                throw new Exception();
                             }
                         }
                     }
@@ -85,14 +85,14 @@ public class SystemOfLinearEquations
                     {
                         System.err.println("error: the file '" + path + "' doesn't provide a square coefficient " +
                                 "matrix with the rank of '" + rank + "'.");
-                        System.exit(1);
+                        throw new Exception();
                     }
                 }
                 else
                 {
                     System.err.println("error: the '" + (i + 2) + "'th line of the file '" + path + "' is null or " +
                             "empty.");
-                    System.exit(1);
+                    throw new Exception();
                 }
             }
 
@@ -101,7 +101,7 @@ public class SystemOfLinearEquations
             {
                 for(Integer value : rows)
                 {
-                    System.out.print(value + " ");
+                    System.out.format("%12d", value);
                 }
                 System.out.println();
             }
@@ -109,7 +109,7 @@ public class SystemOfLinearEquations
             input.constants = new Integer[rank];
             if((line = reader.readLine()) != null && !line.isEmpty())
             {
-                String[] tokens = line.split("\\s+");
+                String[] tokens = line.trim().split("\\s+");
                 if(tokens.length == rank)
                 {
                     for(int i = 0; i < rank; i++)
@@ -124,7 +124,7 @@ public class SystemOfLinearEquations
                                     "structured properly.\n the last line should represent the values of the " +
                                     "constants vector.\n the values must be integer numbers with the min value of '"
                                     + Integer.MIN_VALUE + "' and the max value of '" + Integer.MAX_VALUE + "'.");
-                            System.exit(1);
+                            throw new Exception();
                         }
                     }
                 }
@@ -132,22 +132,20 @@ public class SystemOfLinearEquations
                 {
                     System.err.println("error: the last needed line of the file '" + path + "' doesn't provide a " +
                             "constants vector of the length of '" + rank + "'.");
-                    System.exit(1);
+                    throw new Exception();
                 }
             }
             else
             {
                 System.err.println("error: the last needed line of the file '" + path + "' is null or empty.");
-                System.exit(1);
+                throw new Exception();
             }
 
             System.out.println("the constants vector is as below:");
             for(Integer value : input.constants)
             {
-                System.out.println(value);
+                System.out.format("%12d\n", value);
             }
-
-            return input;
         }
         catch (FileNotFoundException e)
         {
@@ -159,11 +157,18 @@ public class SystemOfLinearEquations
             System.err.println("error: an error occurred while reading from the file '" + path + "'.");
             System.exit(1);
         }
+        catch (Exception e)
+        {
+            System.exit(1);
+        }
         finally
         {
             try
             {
-                reader.close();
+                if(reader != null)
+                {
+                    reader.close();
+                }
             }
             catch (IOException e)
             {
@@ -172,15 +177,49 @@ public class SystemOfLinearEquations
             }
         }
 
-        System.err.println("error: the program should never reach this line.");
-        System.exit(1);
+        return input;
+    }
 
-        return null;
+    private Integer getDeterminant(Integer[][] matrix)
+    {
+        Integer determinant = 0;
+
+        if(matrix.length == 1)
+        {
+            return matrix[0][0];
+        }
+        else
+        {
+            for(int j = 0; j < matrix.length; j++)
+            {
+                Integer[][] minor = new Integer[matrix.length - 1][matrix.length - 1];
+
+                for(int row = 1; row < matrix.length; row++)
+                {
+                    for(int col = 0; col < matrix.length; col++)
+                    {
+                        if(col < j)
+                        {
+                            minor[row - 1][col] = matrix[row][col];
+                        }
+                        else if(col > j)
+                        {
+                            minor[row - 1][col - 1] = matrix[row][col];
+                        }
+                    }
+                }
+
+                determinant += matrix[0][j] * new Double(Math.pow(-1, j)).intValue() * getDeterminant(minor);
+            }
+        }
+        return determinant;
     }
 
     private void solve()
     {
         Input input = read(path);
+        Integer determinant = getDeterminant(input.coefficients);
+        System.out.println("the determinant of the coefficients matrix is '" + determinant + "'.");
     }
 
     private class Input
